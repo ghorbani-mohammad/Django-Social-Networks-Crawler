fa026f536a8fa46573e660978f048c7b7f3adb1a
Mohammad <mohammad.ghorbani@hotmail.com>
2025-08-15 23:57:31 +0330

    feat(linkedin): add JobViewset

--- social/linkedin/urls.py
+++ social/linkedin/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import IgnoredJobViewSet
+from .views import IgnoredJobViewSet, JobViewSet
 
 router = SimpleRouter()
+router.register("job", JobViewSet, basename="job")
 router.register("ignored-job", IgnoredJobViewSet, basename="ignored-job")
 
 urlpatterns = []
--- social/linkedin/views.py
+++ social/linkedin/views.py
@@ -2,9 +2,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters as rf_filters
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import IgnoredJob
+from .models import IgnoredJob, Job
 from .permissions import HasPublicAPIKey
-from .serializers import IgnoredJobSerializer
+from .serializers import IgnoredJobSerializer, JobSerializer
 
 
 class IgnoredJobViewSet(ReadOnlyModelViewSet):
@@ -24,5 +24,19 @@ class IgnoredJobViewSet(ReadOnlyModelViewSet):
         "reason",
         "description",
     ]
+    ordering_fields = ["created_at", "updated_at"]
+    filterset_fields = ["language", "company", "location", "reason"]
+
+
+class JobViewSet(ReadOnlyModelViewSet):
+    queryset = Job.objects.order_by("-id")
+    serializer_class = JobSerializer
+    permission_classes = [HasPublicAPIKey]
+    filter_backends = [
+        DjangoFilterBackend,
+        rf_filters.SearchFilter,
+        rf_filters.OrderingFilter,
+    ]
+    search_fields = ["title", "company", "location", "description"]
     ordering_fields = ["created_at", "updated_at"]
     filterset_fields = ["language", "company", "location", "reason"]
