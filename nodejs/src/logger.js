e6d39114d1f8d6c2c3d9c570869810dcafa33370
Mohammad <mohammad.ghorbani@hotmail.com>
2025-09-02 19:29:10 +0330

    chore(logging): update log filenames to include 'nodejs' prefix

--- nodejs/src/logger.js
+++ nodejs/src/logger.js
@@ -22,14 +22,14 @@ const logger = winston.createLogger({
   transports: [
     // Write all logs with level 'error' and below to error.log
     new winston.transports.File({ 
-      filename: path.join(logsDir, 'error.log'), 
+      filename: path.join(logsDir, 'nodejs_error.log'), 
       level: 'error',
       maxsize: 5242880, // 5MB
       maxFiles: 5
     }),
     // Write all logs with level 'info' and below to combined.log
     new winston.transports.File({ 
-      filename: path.join(logsDir, 'combined.log'),
+      filename: path.join(logsDir, 'nodejs_combined.log'),
       maxsize: 5242880, // 5MB
       maxFiles: 5
     }),
