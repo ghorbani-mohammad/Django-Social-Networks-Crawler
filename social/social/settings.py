1310e9c67f58a46dd45f0a47bed3b251b79acdb5
Mohammad <mohammad.ghorbani@hotmail.com>
2025-09-06 22:47:31 +0330

    wip(settings): update EMAIL_HOST to use environment variable for better configuration management

--- social/social/settings.py
+++ social/social/settings.py
@@ -167,7 +167,7 @@ CACHES = {
 # Email Configs
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp-mail.outlook.com"
+EMAIL_HOST = env("EMAIL_HOST", default=None)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default=None)
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default=None)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
